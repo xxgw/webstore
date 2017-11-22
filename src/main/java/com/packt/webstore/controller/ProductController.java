92ba78a4769a89b89bdcb450454154bdb34eee20
Steven Yang <xxgw@sina.com>
2017-11-22 23:02:45 +0800

    add repository codes

---
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.packt.webstore.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
+
+@Controller
+public class ProductController {
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@RequestMapping("/products")
+	public String list(Model model) {
+//		Product iphone = new Product("P1234","iPhone 6s", new BigDecimal(500));
+//		iphone.setDescription("Apple iPhone 6s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
+//		iphone.setCategory("Smartphone");
+//		iphone.setManufacturer("Apple");
+//		iphone.setUnitsInStock(1000);
+//		model.addAttribute("product", iphone);
+		model.addAttribute("product", productRepository.getAllProducts());
+		return "products";
+	}
+}
