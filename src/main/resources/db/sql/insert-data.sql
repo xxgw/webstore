c6e79ead2ea98fec4666fd66ffde646c14145fe5
杨志俊 <zhijun.yang@ccblife.com.cn>
2017-11-23 16:22:57 +0800

    update messy code

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -8,5 +8,5 @@ Core processors',700,'Dell','Laptop','New',1000,0,false);
 
 INSERT INTO PRODUCTS VALUES ('P1236', 'Nexus 7', 'Google
 Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm
-Snapdragon™ S4 Pro processor',
+Snapdragon S4 Pro processor',
 300,'Google','Tablet','New',1000,0,false);
